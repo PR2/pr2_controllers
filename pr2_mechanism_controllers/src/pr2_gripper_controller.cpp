6d02410188e58fb0ce173a7151f3956dca1d5e1e
sglaser <sglaser@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-12-31 01:31:42 +0000

    The gripper controller now checks that its joint is calibrated.

--- pr2_mechanism_controllers/src/pr2_gripper_controller.cpp
+++ pr2_mechanism_controllers/src/pr2_gripper_controller.cpp
@@ -77,6 +77,13 @@ bool Pr2GripperController::init(pr2_mechanism_model::RobotState *robot, ros::Nod
     return false;
   }
 
+  if (!joint_state_->calibrated_)
+  {
+    ROS_ERROR("Joint %s is not calibrated (namespace: %s)",
+              joint_state_->joint_->name.c_str(), node_.getNamespace().c_str());
+    return false;
+  }
+
   if (!pid_.init(ros::NodeHandle(node_, "pid")))
     return false;
 
