8341cf4e8b8875dbdf60b8b59cea4493b0931a78
sglaser <sglaser@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-04-03 00:32:22 +0000

    Reverting to old base controller to maintain backwards compatibility.

--- pr2_mechanism_controllers/include/pr2_mechanism_controllers/pr2_base_controller.h
+++ pr2_mechanism_controllers/include/pr2_mechanism_controllers/pr2_base_controller.h
@@ -91,7 +91,7 @@ namespace controller
        * \brief class where the robot's information is computed and stored
        * @return BaseKinematic instance that is being used by this controller
        */
-      BaseKinematics base_kinematics_;
+      BaseKinematics base_kin_;
 
       /*!
        * \brief mutex lock for setting and getting commands
@@ -223,7 +223,7 @@ namespace controller
       /*!
        * \brief computes the desired wheel speeds given the desired base speed
        */
-      void computeDesiredWheelSpeeds(const double &dT);
+      void computeDesiredWheelSpeeds();
 
       /*!
        * \brief sends the desired wheel speeds to the wheel controllers
@@ -286,15 +286,6 @@ namespace controller
       filters::MultiChannelTransferFunctionFilter<double> caster_vel_filter_;
       
       std::vector<double> filtered_velocity_;
-
-      filters::MultiChannelTransferFunctionFilter<double> wheel_vel_filter_;
-      
-      std::vector<double> filtered_wheel_velocity_;
-
-      /*!
-       * \brief The pid controllers for caster position
-       */
-      std::vector<control_toolbox::Pid> wheel_pid_controllers_;
   };
 
 }
