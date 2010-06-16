9a611a58861ecd68f1c3d1474f7e003cc8aa517a
sglaser <sglaser@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-06-16 21:45:44 +0000

    Created the fake calibration controller, which just marks joints to calibrated.

---
+++ pr2_calibration_controllers/src/fake_calibration_controller.cpp
@@ -0,0 +1,105 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include "pr2_calibration_controllers/fake_calibration_controller.h"
+#include "ros/time.h"
+#include "pluginlib/class_list_macros.h"
+
+using namespace std;
+using namespace controller;
+
+PLUGINLIB_REGISTER_CLASS(FakeCalibrationController, controller::FakeCalibrationController, pr2_controller_interface::Controller)
+
+namespace controller
+{
+
+FakeCalibrationController::FakeCalibrationController()
+  : last_publish_time_(0), joint_(NULL)
+{
+}
+
+FakeCalibrationController::~FakeCalibrationController()
+{
+}
+
+bool FakeCalibrationController::init(pr2_mechanism_model::RobotState *robot,
+                                        ros::NodeHandle &n)
+{
+  assert(robot);
+  robot_ = robot;
+  node_ = n;
+
+  std::string joint_name;
+  if (!node_.getParam("joint", joint_name))
+  {
+    ROS_ERROR("No joint given (namespace: %s)", node_.getNamespace().c_str());
+    return false;
+  }
+  if (!(joint_ = robot->getJointState(joint_name)))
+  {
+    ROS_ERROR("Could not find joint \"%s\" (namespace: %s)",
+              joint_name.c_str(), node_.getNamespace().c_str());
+    return false;
+  }
+
+  // advertise service to check calibration
+  is_calibrated_srv_ = node_.advertiseService("is_calibrated", &FakeCalibrationController::isCalibrated, this);
+
+  // "Calibrated" topic
+  pub_calibrated_.reset(new realtime_tools::RealtimePublisher<std_msgs::Empty>(node_, "calibrated", 1));
+
+  return true;
+}
+
+
+void FakeCalibrationController::starting()
+{
+}
+
+
+bool FakeCalibrationController::isCalibrated(pr2_controllers_msgs::QueryCalibrationState::Request& req,
+						pr2_controllers_msgs::QueryCalibrationState::Response& resp)
+{
+  resp.is_calibrated = joint_->calibrated_;
+  return true;
+}
+
+
+void FakeCalibrationController::update()
+{
+  assert(joint_);
+  joint_->calibrated_ = true;
+}
+
+}
